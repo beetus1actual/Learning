4be574aaf6b48bad173b58bf2d72925f27d788f3
Chance Benson <chance@mcbhosting.us>
2025-07-30 11:22:32 -0500

    lesson 38 why tuples and never use them

--- features/tuples.ts
+++ features/tuples.ts
@@ -4,8 +4,16 @@
     sugar: 40
 };
 
+// Type alias
 type Drink = [string, boolean, number];
 
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStates = {
+    horsepower: 400,
+    weight: 3354
+};
