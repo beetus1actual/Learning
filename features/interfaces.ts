e9a49f211f7a15b3e2d275372b61c5dbe6b14d06
Chance Benson <chance@mcbhosting.us>
2025-08-01 09:43:41 -0500

    lesson 38-44 learning interfacing

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,13 +1,12 @@
-﻿const oldCivic = {
+﻿interface Reportable {
+    summary(): string;
+}
+
+const oldCivic = {
     name: 'civic',
-    year: 2000,
-    broken: true
+    year: new Date(),
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`;
+    }
 };
-
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
-    console.log(`Name: ${vehicle.name}`);
-    console.log(`Year: ${vehicle.year}`);
-    console.log(`Broken? ${vehicle.broken}`);
-};
-
-printVehicle(oldCivic);
