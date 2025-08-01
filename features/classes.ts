6a40d06cf977912da610dc671cd18b2487b3a62e
Chance Benson <chance@mcbhosting.us>
2025-08-01 09:39:52 -0500

    lesson 46 class inheritance

---
+++ features/classes.ts
@@ -0,0 +1,13 @@
+﻿class Vehicle {
+    drive(): void {
+        console.log('chugga chugga');
+    }
+    
+    honk(): void {
+        console.log('beep');
+    }
+}
+
+const vehicle: Vehicle = new Vehicle();
+vehicle.drive();
+vehicle.honk();
